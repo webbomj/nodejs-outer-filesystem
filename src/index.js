86a86b26dae8f33cdd5b150389aa529514b150ef
webbomj <nethack@mail.ru>
2023-06-20 23:56:00 +0300

    feat: add hash

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { getUserNameByArg } from "./helpers/getUserNameByArg.js";
 import { getCPUS } from "./services/os/cpus.js";
 import { getHomedir } from "./services/os/homedir.js";
 import { getArch } from "./services/os/architecture.js";
+import { hashInterpret } from "./services/interpretator/hashInterpretator.js";
+import { getHash } from "./services/hash/hash.js";
 
 const userName = getUserNameByArg(process.argv[2]);
 console.log(`Welcome to the File Manager, ${userName}`);
@@ -18,28 +20,34 @@ console.log(`You are currently in ${process.cwd()}`);
 
 //up, cd,  ls
 //cat, add , rn, cp, mv, rm
-//EOL, cpus, homedir, username, architecture
 //hash
 //compress, decompress
 
-rl.on("line", (line) => {
-  switch (line.trim()) {
-    case "os --EOL":
-      getEOL();
-      break;
-    case "os --cpus":
-      getCPUS();
-      break;
-    case "os --homedir":
-      getHomedir();
-      break;
-    case "os --architecture":
-      getArch();
-      break;
+rl.on("line", async (line) => {
+  const trimLine = line.trim();
+  const [cmd, arg1, arg2] = trimLine.split(" ");
 
+  console.log(trimLine, cmd, arg1, arg2);
+  switch (cmd) {
+    // case "os --EOL":
+    //   getEOL();
+    //   break;
+    // case "os --cpus":
+    //   getCPUS();
+    //   break;
+    // case "os --homedir":
+    //   getHomedir();
+    //   break;
+    // case "os --architecture":
+    //   getArch();
+    //   break;
+    case "hash":
+      await getHash(arg1);
+      break;
     default:
       break;
   }
+
   console.log(`You are currently in ${process.cwd()}`);
 });
 
